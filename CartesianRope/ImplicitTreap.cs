3ebf0ca5b827374f9f01133d96160900401569f3
Flux <gmFlux@gmail.com>
2017-01-10 05:57:26 +0200

    First roll of cartesian tree and optimality benchmark

---
+++ CartesianRope/ImplicitTreap.cs
@@ -0,0 +1,71 @@
+﻿namespace CartesianRope
+{
+    public class ImplicitTreap
+    {
+        private int y;
+
+        public ImplicitTreap Left;
+        public ImplicitTreap Right;
+
+        public int Length;
+        public int Size;
+        public int Cost;
+
+        public double AverageAccess => (double)Cost / Size; 
+
+
+        public ImplicitTreap(int y, int len, ImplicitTreap left = null, ImplicitTreap right = null)
+        {
+            this.y = y;
+            Left = left;
+            Right = right;
+            Length = len;
+            Size = GetSize(Left) + Length + GetSize(Right);
+            Cost = GetCost(Left) + Size + GetCost(Right);
+        }
+
+        private static int GetSize(ImplicitTreap n) => n == null ? 0 : n.Size;
+        private static int GetCost(ImplicitTreap n) => n == null ? 0 : n.Cost;
+
+        public void Split(int x, out ImplicitTreap L, out ImplicitTreap R)
+        {
+            ImplicitTreap newTree = null;
+            int curIndex = GetSize(Left) + 1;
+
+            if (curIndex <= x)
+            {
+                if (Right == null)
+                    R = null;
+                else
+                    Right.Split(x - curIndex, out newTree, out R);
+                L = new ImplicitTreap(y, Length, Left, newTree);
+            }
+            else
+            {
+                if (Left == null)
+                    L = null;
+                else
+                    Left.Split(x, out L, out newTree);
+                R = new ImplicitTreap(y, Length, newTree, Right);
+            }
+        }
+
+        public static ImplicitTreap Merge(ImplicitTreap L, ImplicitTreap R)
+        {
+            if (L == null) return R;
+            if (R == null) return L;
+
+            if (L.y > R.y)
+            {
+                var newR = Merge(L.Right, R);
+                return new ImplicitTreap(L.y, L.Length, L.Left, newR);
+            }
+            else
+            {
+                var newL = Merge(L, R.Left);
+                return new ImplicitTreap(R.y, R.Length, newL, R.Right);
+            }
+        }
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using CartesianRope;
+
+namespace Test
+{
+    class Program
+    {
+        static Random rnd = new Random(1);
+
+        static double GetAdvantage(int arrSize, int minLength, int maxLength)
+        {
+            int[] arr = new int[arrSize];
+            for (int i = 0; i < arrSize; i++)
+                arr[i] = minLength + rnd.Next() % (maxLength - minLength);
+
+            var pureNode = NodeHelper.ConstructOptimal(arr);
+            var implNode = arr.Select(x => new ImplicitTreap(rnd.Next(), x)).Aggregate(ImplicitTreap.Merge);
+            // do testing
+            return implNode.AverageAccess / pureNode.AverageAccess;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            const int testCount = 100;
+            const int arrSize = 500;
+
+            double min = double.MaxValue, max = double.MinValue;
+            for (int i = 1; i <= testCount; i++)
+            {
+                double testResult = GetAdvantage(arrSize, 1, 5000);
+                min = Math.Min(min, testResult);
+                max = Math.Max(max, testResult);
+                //Console.WriteLine("{0}\t/\t{1}", i, testCount);
+            }
+            Console.WriteLine("Optimal is better in {0} - {1} times.", min, max);
+        }
+    }
+}
