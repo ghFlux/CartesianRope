c1030834168f754ad2a8285b0780447171255d5b
root <gmFlux@gmail.com>
2017-04-11 23:10:09 +0300

    Moved TreapNode class into separate file, refactorings.

--- CartesianRope/ImplicitTreap.cs
+++ CartesianRope/ImplicitTreap.cs
@@ -2,80 +2,12 @@
 
 using System.Diagnostics;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace CartesianRope
 {
     [DebuggerDisplay("{GetSize(Left)} - [{Length}] - {GetSize(Right)}")]
-    public partial class TreapNode<T>
-    {
-        public int Priority { get; }
-
-        public TreapNode<T> Left { get; internal set; }
-        public TreapNode<T> Right { get; internal set; }
-
-        public int Length { get; }
-        public int Size { get; }
-        public int Cost { get; }
-        public T[] Data { get; }
-
-        public double AverageAccess => (double)Cost / Size;
-
-        public TreapNode(T[] data, int priority, int len, TreapNode<T> left = null, TreapNode<T> right = null)
-        {
-            Data = data;
-            Priority = priority;
-            Left = left;
-            Right = right;
-            Length = len;
-            Size = GetSize(Left) + Length + GetSize(Right);
-            Cost = GetCost(Left) + Size + GetCost(Right);
-        } 
-
-        public static int GetSize(TreapNode<T> node) => node == null ? 0 : node.Size;
-        public static int GetCost(TreapNode<T> node) => node == null ? 0 : node.Cost;
-
-        public static TreapNode<T> ConstructOptimal(TreapNode<T>[] arr)
-        {
-            int n = arr.Length;
-            TreapNode<T>[,] cache = new TreapNode<T>[n, n];
-            Func<int, int, TreapNode<T>> construct = null;
-            construct = (from, to) =>
-            {
-                if (from > to) return null;
-                if (from == to) return arr[from];
-                if (cache[from, to] != null) return cache[from, to];
-
-                TreapNode<T> res = null;
-                {
-                    int bestCost = int.MaxValue;
-                    for (int split = from; split <= to; split++)
-                    {
-                        TreapNode<T> tmp = new TreapNode<T>(null, arr[split].Priority, arr[split].Length, construct(from, split - 1), construct(split + 1, to));
-                        if (tmp.Cost < bestCost) bestCost = (res = tmp).Cost;
-                    }
-                }
-                return cache[from, to] = res;
-            };
-            return construct(0, n - 1);
-        }
-        
-        public TreapNode<T>[] Flatten()
-        {
-            List<TreapNode<T>> result = new List<TreapNode<T>>();
-            Action<TreapNode<T>> flatten = null;
-            flatten = (node) =>
-            {
-                if (node == null) return;
-                flatten(node.Left);
-                result.Add(new TreapNode<T>(null, node.Priority, node.Length));
-                flatten(node.Right);
-            };
-            flatten(this);
-            return result.ToArray();
-        }
-    }
-
     public sealed partial class Rope<T>
     {
         internal TreapNode<T> Root { get; set; }
@@ -98,70 +30,122 @@ namespace CartesianRope
 
             if (L.Priority > R.Priority)
             {
-                var newR = Merge(L.Right, R);
-                return new TreapNode<T>(null, L.Priority, L.Length, L.Left, newR);
+                var newR = Merge(L.rChild, R);
+                return new TreapNode<T>(null, L.Priority, L.Length, L.lChild, newR);
             }
             else
             {
-                var newL = Merge(L, R.Left);
-                return new TreapNode<T>(null, R.Priority, R.Length, newL, R.Right);
+                var newL = Merge(L, R.lChild);
+                return new TreapNode<T>(null, R.Priority, R.Length, newL, R.rChild);
             }
         }
 
+        public static void Traverse(TreapNode<T> node, Action<TreapNode<T>> action)
+        {
+            if (node == null) return;
+            Traverse(node.lChild, action);
+            action(node);
+            Traverse(node.rChild, action);
+        }
+
+        /// <summary>
+        /// Splits node content into two trees by index.
+        /// </summary>
+        /// <param name="node">Node to split</param>
+        /// <param name="index">Splitting index</param>
+        /// <param name="L">Left resulting tree</param>
+        /// <param name="R">Right resulting tree</param>
         public void Split(TreapNode<T> node, int index, out TreapNode<T> L, out TreapNode<T> R)
         {
             TreapNode<T> newTree = null;
-            int leftSize = TreapNode<T>.GetSize(node.Left);
+            int lBound = TreapNode<T>.GetSize(node.lChild);
+            int rBound = lBound + node.Size;
 
-            if (leftSize < index) // going right
+            if (index >= rBound) // going right
             {
-                if (node.Right == null)
+                if (node.rChild == null)
                     R = null;
                 else
-                    Split(node.Right, index - leftSize, out newTree, out R);
+                    Split(node.rChild, index - rBound, out newTree, out R);
 
-                L = new TreapNode<T>(node.Data, node.Priority, node.Length, node.Left, newTree);
+                L = new TreapNode<T>(node.Data, node.Priority, node.Length, node.lChild, newTree);
             }
-            else // going left
+            else if (index < lBound) // going left
             {
-                if (node.Left == null)
+                if (node.lChild == null)
                     L = null;
                 else
-                    Split(node.Left, index, out L, out newTree);
+                    Split(node.lChild, index, out L, out newTree);
 
-                R = new TreapNode<T>(node.Data, node.Priority, node.Length, newTree, node.Right);
+                R = new TreapNode<T>(node.Data, node.Priority, node.Length, newTree, node.rChild);
+            }
+            else // split node in middle
+            {
+                index -= lBound;
+                L = new TreapNode<T>(data: node.Data, priority: RandomGenerator.Next(), len: index, left: node.lChild, right: null);
+                R = new TreapNode<T>(data: node.Data, priority: RandomGenerator.Next(), len: node.Length - index, left: null, right: node.rChild);
             }
         }
 
-        private void SplitNode(TreapNode<T> node, int index, out TreapNode<T> L, out TreapNode<T> R)
-        {
-            L = new TreapNode<T>(data: node.Data, priority: RandomGenerator.Next(), len: index);
-            R = new TreapNode<T>(data: node.Data, priority: RandomGenerator.Next(), len: node.Length - index);
-        }
-
-        private void Split2(TreapNode<T> node, int index, out TreapNode<T> L, out TreapNode<T> R)
+        /// <summary>
+        /// Splits node content into two trees by index. Uses stack instead of recursion.
+        /// </summary>
+        /// <param name="node">Node to split</param>
+        /// <param name="index">Splitting index</param>
+        /// <param name="L">Left resulting tree</param>
+        /// <param name="R">Right resulting tree</param>
+        private void SplitStack(TreapNode<T> node, int index, out TreapNode<T> L, out TreapNode<T> R)
         {
             Stack<TreapNode<T>> path = new Stack<TreapNode<T>>();
+            L = R = null;
 
             while (true)
             {
                 path.Push(node);
 
-                int leftBound = TreapNode<T>.GetSize(node.Left);
+                int leftBound = TreapNode<T>.GetSize(node.lChild);
                 int rightBound = leftBound + node.Size;
 
-                if (index < leftBound)
+                if (index < leftBound) // going left
                 {
-                    
+                    node = node.lChild;
                 }
-                else if (rightBound <= index)
+                else if (rightBound <= index) // going right
                 {
+                    node = node.rChild;
                 }
-                else
+                else // split node
                 {
+                    L = new TreapNode<T>(
+                            data: node.Data,
+                            priority: RandomGenerator.Next(),
+                            len: index,
+                            left: node.lChild,
+                            right: null
+                        );
+                    R = new TreapNode<T>(
+                            data: node.Data,
+                            priority: RandomGenerator.Next(),
+                            len: node.Length - index,
+                            left: null,
+                            right: node.rChild
+                        );
+                }
+                while (path.Count > 0)
+                {
+                    var currentNode = path.Pop();
+
                 }
 
+                Console.WriteLine(string.Join(" - ", path.Select(p => p.Size)));
             }
+        }
+
+        public bool Equals(IEnumerable<T> other)
+        {
+            List<T> buffer = new List<T>();
+            Traverse(Root, node => buffer.AddRange(node.Data.Take(node.Length)));
+            return buffer.SequenceEqual(other);
         }
     }
 }
---
+++ CartesianRope/TreapNode.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace CartesianRope
+{
+    /// <summary>
+    /// Node of cartesian tree.
+    /// </summary>
+    /// <typeparam name="T">Item type in container</typeparam>
+    [DebuggerDisplay("{GetSize(Left)} - [{Length}] - {GetSize(Right)}")]
+    public partial class TreapNode<T>
+    {
+        public int Priority { get; }
+
+        public TreapNode<T> lChild { get; internal set; }
+        public TreapNode<T> rChild { get; internal set; }
+
+        public int Length { get; }
+        public int Size { get; }
+        public int Cost { get; }
+        public T[] Data { get; }
+
+        public double AverageAccess => (double)Cost / Size;
+
+        /// <summary>
+        /// Constructs new instance of node class.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="priority"></param>
+        /// <param name="len"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        public TreapNode(T[] data, int priority, int len, TreapNode<T> left = null, TreapNode<T> right = null)
+        {
+            Data = data;
+            Priority = priority;
+            lChild = left;
+            rChild = right;
+            Length = len;
+            Size = GetSize(lChild) + Length + GetSize(rChild);
+            Cost = GetCost(lChild) + Size + GetCost(rChild);
+        }
+
+        public static int GetSize(TreapNode<T> node) => node == null ? 0 : node.Size;
+        public static int GetCost(TreapNode<T> node) => node == null ? 0 : node.Cost;
+
+        public static TreapNode<T> ConstructOptimal(TreapNode<T>[] arr)
+        {
+            int n = arr.Length;
+            TreapNode<T>[,] cache = new TreapNode<T>[n, n];
+            Func<int, int, TreapNode<T>> construct = null;
+            construct = (from, to) =>
+            {
+                if (from > to) return null;
+                if (from == to) return arr[from];
+                if (cache[from, to] != null) return cache[from, to];
+
+                TreapNode<T> res = null;
+                {
+                    int bestCost = int.MaxValue;
+                    for (int split = from; split <= to; split++)
+                    {
+                        TreapNode<T> tmp = new TreapNode<T>(null, arr[split].Priority, arr[split].Length, construct(from, split - 1), construct(split + 1, to));
+                        if (tmp.Cost < bestCost) bestCost = (res = tmp).Cost;
+                    }
+                }
+                return cache[from, to] = res;
+            };
+            return construct(0, n - 1);
+        }
+
+        public TreapNode<T>[] Flatten()
+        {
+            List<TreapNode<T>> result = new List<TreapNode<T>>();
+            Rope<T>.Traverse(this, node => result.Add(new TreapNode<T>(node.Data, node.Priority, node.Length)));
+            return result.ToArray();
+        }
+    }
+}
